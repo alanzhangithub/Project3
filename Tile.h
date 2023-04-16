08cf9bb0b25b2230573ad686d73b68bda9ce126a
Alan <alanzhang@ufl.edu>
2023-04-16 10:26:08 -0400

    added stuff

---
+++ Tile.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <map>
+using namespace sf;
+using namespace std;
+
+class Tile {
+    Sprite background;
+    Sprite foreground;
+    Sprite icon;
+    Sprite flagicon;
+    bool isdebug;
+
+    void setIconTexture(map<string, Texture> &textures);
+
+public:
+    vector<Tile*> neighbors;
+    int numbombs;
+    bool isbomb;
+    bool isflagged;
+    bool isrevealed;
+
+    Tile(map<string, Texture> &textures, float x_coordinate, float y_coordinate);
+    void setBombState(bool bombstate);
+    void updateIcon(map<string, Texture> &textures);
+
+    bool toggleFlag();
+    void toggleDebug();
+    bool reveal(int &counter);
+    void draw(RenderWindow &w);
+};
+
+Tile::Tile(map<string, Texture> &textures, float x_coordinate, float y_coordinate) {
+    background.setPosition(x_coordinate, y_coordinate);
+    foreground.setPosition(x_coordinate, y_coordinate);
+    flagicon.setPosition(x_coordinate, y_coordinate);
+    icon.setPosition(x_coordinate, y_coordinate);
+
+    background.setTexture(textures["revealed"]);
+    foreground.setTexture(textures["hidden"]);
+    flagicon.setTexture(textures["flag"]);
+
+    flagicon.setColor(Color(255, 255, 255, 0));
+    isbomb = false;
+    isrevealed = false;
+    isflagged = false;
+    isdebug = false;
+}
+
+void Tile::setBombState(bool bombstate) {
+    isbomb = bombstate;
+}
+
+void Tile::updateIcon(map<string, Texture> &textures) {
+    numbombs = 0;
+    for (Tile* neighbor : neighbors) {
+        if (neighbor != nullptr && neighbor->isbomb) {
+            numbombs++;
+        }
+    }
+    setIconTexture(textures);
+    icon.setColor(Color(255, 255, 255, 0));
+    flagicon.setColor(Color(255, 255, 255, 0));
+    foreground.setColor(Color(255, 255, 255, 255));
+    isrevealed = false;
+    isflagged = false;
+    isdebug = false;
+}
+
+void Tile::setIconTexture(map<string, Texture> &textures) {
+    if (isbomb) {
+        icon.setTexture(textures["bomb"]);
+    } else {
+        static const string numberTextures[] = {"revealed", "one", "two", "three", "four", "five", "six", "seven", "eight"};
+        icon.setTexture(textures[numberTextures[numbombs]]);
+    }
+}
+
+bool Tile::toggleFlag() {
+    if (!isrevealed) {
+        isflagged = !isflagged;
+        flagicon.setColor(isflagged ? Color(255, 255, 255, 255) : Color(255, 255, 255, 0));
+    }
+    return isflagged;
+}
+
+void Tile::toggleDebug() {
+    if (isbomb && !isrevealed) {
+        isdebug = !isdebug;
+        icon.setColor(isdebug ? Color(255, 255, 255, 255) : Color(255, 255, 255, 0));
+    }
+}
+
+bool Tile::reveal(int &counter) {
+    if (!isrevealed && !isflagged) {
+        isrevealed = true;
+        counter++;
+        foreground.setColor(Color(255, 255, 255, 0));
+        icon.setColor(Color(255, 255, 255, 255));
+        if (numbombs == 0 && !isbomb) {
+            for (Tile* neighbor : neighbors) {
+                if (neighbor != nullptr && !(neighbor->isbomb)) {
+                    neighbor->reveal(counter);
+                }
+            }
+        }
+    }
+    return isbomb;
+}
+
+void Tile::draw(RenderWindow &w) {
+    w.draw(background);
+    w.draw(foreground);
+    w.draw(icon);
+    w.draw(flagicon);
+}
--- main.cpp
+++ main.cpp
@@ -1,36 +1,64 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <vector>
+#include <cctype>
 #include "Tile.h"
 #include "Functions.h"
 using namespace sf;
 using namespace std;
 
-// Add an enumeration for game states
 enum GameState {
     WELCOME_SCREEN,
     PLAYING,
 };
+
+void setText(sf::Text &text, float x, float y) {
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width / 2.0f,
+                   textRect.top + textRect.height / 2.0f);
+    text.setPosition(sf::Vector2f(x, y));
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 576), "Minesweeper");
 
     Controller gamemaster;
 
-    // Create a variable for the current game state and set it to the welcome screen
     GameState currentState = WELCOME_SCREEN;
 
-    // Create a welcome screen text object
     sf::Font font;
     if (!font.loadFromFile("font.ttf")) {
-        return -1; // Failed to load font
+        return -1;
     }
+
+    // Welcome Text
     sf::Text welcomeText;
     welcomeText.setFont(font);
-    welcomeText.setString("Welcome to Minesweeper!\nPress Enter to start.");
+    welcomeText.setString("WELCOME TO MINESWEEPER!");
     welcomeText.setCharacterSize(24);
-    welcomeText.setFillColor(sf::Color::Black);
-    welcomeText.setPosition(200, 200);
+    welcomeText.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    welcomeText.setFillColor(sf::Color::White);
+    setText(welcomeText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 150);
+
+    // Input Prompt Text
+    sf::Text inputPromptText;
+    inputPromptText.setFont(font);
+    inputPromptText.setString("Enter your name:");
+    inputPromptText.setCharacterSize(20);
+    inputPromptText.setStyle(sf::Text::Bold);
+    inputPromptText.setFillColor(sf::Color::White);
+    setText(inputPromptText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 75);
+
+    // Input Text
+    sf::Text inputText;
+    inputText.setFont(font);
+    inputText.setString("");
+    inputText.setCharacterSize(18);
+    inputText.setStyle(sf::Text::Bold);
+    inputText.setFillColor(sf::Color::Yellow);
+    setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
+
+    std::string playerName;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -39,22 +67,40 @@ int main() {
                 window.close();
             }
 
-            // Handle key presses
-            if (event.type == sf::Event::KeyPressed) {
-                if (currentState == WELCOME_SCREEN && event.key.code == sf::Keyboard::Enter) {
-                    currentState = PLAYING;
+            if (currentState == WELCOME_SCREEN) {
+                if (event.type == sf::Event::TextEntered) {
+                    if (std::isalpha(event.text.unicode) &&
+                        playerName.length() < 10) {
+                        char enteredChar = static_cast<char>(event.text.unicode);
+                        playerName += (playerName.empty()) ? std::toupper(enteredChar) : std::tolower(enteredChar);
+                        inputText.setString(playerName + "|");
+                        setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
+                    }
+                }
+
+                if (event.type == sf::Event::KeyPressed) {
+                    if (event.key.code == sf::Keyboard::BackSpace && !playerName.empty()) {
+                        playerName.pop_back();
+                        inputText.setString(playerName + "|");
+                        setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
+                    } else if (event.key.code == sf::Keyboard::Enter && !playerName.empty()) {
+                        currentState = PLAYING;
+                    }
                 }
             }
         }
 
-        window.clear(Color(255, 255, 255, 255));
+        window.clear(sf::Color::Blue);
 
-        // Use a switch-case statement to handle different game states
         switch (currentState) {
             case WELCOME_SCREEN:
+                window.clear(sf::Color::Blue);
                 window.draw(welcomeText);
+                window.draw(inputPromptText);
+                window.draw(inputText);
                 break;
             case PLAYING:
+                window.clear(sf::Color::White); // Clear with white color when in PLAYING state
                 gamemaster.update(window);
                 break;
         }
@@ -64,3 +110,5 @@ int main() {
 
     return 0;
 }
+//milestone 1 graded by Nikhil
+//milestone 2 graded by Kidane Chinn
