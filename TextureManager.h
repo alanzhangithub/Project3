4d34a65ccd14f60c62bc9c834b47b5d06d310e50
Alan <alanzhang@ufl.edu>
2023-04-18 23:37:45 -0400

    weird

--- TextureManager.h
+++ TextureManager.h
@@ -1,38 +1,549 @@
-#ifndef TEXTURE_MANAGER_H
-#define TEXTURE_MANAGER_H
+//
+// Created by Alan on 4/11/2023.
+//
 
+#pragma once
+#include "Tile.h"
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 #include <map>
-#include <string>
+#include <iostream>
+#include <fstream>
 
-class TextureManager
-{
+#include <iomanip>
+using namespace std;
+using namespace sf;
+using namespace std::chrono;
+
+
+class TextureManager {
+    int flagsavailable;
+    int tilesrevealed;
+    int columns, rows, mines;
+    int tile_count;
+    int minesLeft;
+
+
+
+    std::map<std::string, sf::Texture> icontextures;
+    vector<bool> tileids;
+
+
+    Sprite debug_icon;
+
+    Sprite smiley;
+    Sprite leaderboard;
+    Sprite pause;
+    Sprite play;
+
+    Sprite hundreds;
+    Sprite tens;
+    Sprite ones;
+    sf::Sprite timerMinutesTens;
+    sf::Sprite timerMinutesOnes;
+    sf::Sprite timerSecondsTens;
+    sf::Sprite timerSecondsOnes;
+    bool game_lose{};
+    bool game_over{};
+
+
 public:
-    // Get the instance of the singleton class
-    static TextureManager& getInstance();
+    int initialMinesLeft = mines;
+    vector<Tile> tiles;
+    TextureManager();
+    void RandomTileIds();
+    void MakeTiles();
+    void setBombStates();
+    void setIcons();
+    void update(RenderWindow &window);
+    void debug();
+    void draw(RenderWindow &window);
+    void refreshCounter();
+    void refreshSmiley();
+    void load_board_config();
+    int width{};
+    int height{};
+    bool rightMouseButtonPressed;
+    bool leftButtonClicked;
+    sf::Clock gameClock;
+    int currentTime;
 
-    // Load a texture from a file and store it with a given identifier
-    bool loadTexture(const std::string& identifier, const std::string& filename);
-
-    // Get a reference to the texture with the given identifier
-    sf::Texture& getTexture(const std::string& identifier);
-
-    // Remove a texture with the given identifier
-    void removeTexture(const std::string& identifier);
-
-// Constructor
-TextureManager() {}
-
-private:
-
-    // Copy constructor (deleted to prevent multiple instances)
-    TextureManager(const TextureManager&) = delete;
-
-    // Assignment operator (deleted to prevent multiple instances)
-    TextureManager& operator=(const TextureManager&) = delete;
-
-    // Map to store textures with their identifier
-    std::map<std::string, sf::Texture> textures;
+    void updateTimer();
 };
 
-#endif // TEXTURE_MANAGER_H
+
+TextureManager::TextureManager()
+{
+    leftButtonClicked = false;
+    rightMouseButtonPressed = false;
+    load_board_config();
+    flagsavailable = mines;
+    minesLeft = mines;
+
+
+
+    load_board_config();
+    std::map<std::string, std::string> TEXTURE_FILES = {
+            {"debug", "images/debug.png"},
+            {"leaderboard", "images/leaderboard.png"},
+            {"play", "images/play.png"},
+            {"pause", "images/pause.png"},
+            {"digits", "images/digits.png"},
+            {"happy", "images/face_happy.png"},
+            {"lose", "images/face_lose.png"},
+            {"win", "images/face_win.png"},
+            {"flag", "images/flag.png"},
+            {"bomb", "images/mine.png"},
+            {"one", "images/number_1.png"},
+            {"two", "images/number_2.png"},
+            {"three", "images/number_3.png"},
+            {"four", "images/number_4.png"},
+            {"five", "images/number_5.png"},
+            {"six", "images/number_6.png"},
+            {"seven", "images/number_7.png"},
+            {"eight", "images/number_8.png"},
+            {"hidden", "images/tile_hidden.png"},
+            {"revealed", "images/tile_revealed.png"}
+    };
+
+    for (const auto& [name, file] : TEXTURE_FILES) {
+        sf::Texture texture;
+        icontextures.emplace(name, texture);
+        icontextures[name].loadFromFile(file);
+    }
+
+    debug_icon.setTexture(icontextures["debug"]);
+    debug_icon.setPosition((columns * 32) - 304, 32 * (rows + 0.5f));
+    smiley.setTexture(icontextures["happy"]);
+    smiley.setPosition(((columns / 2.0) * 32) - 32, 32 * (rows + 0.5f));
+    pause.setTexture(icontextures["pause"]);
+    pause.setPosition((columns * 32) - 240, 32 * (rows + 0.5f));
+    leaderboard.setTexture(icontextures["leaderboard"]);
+    leaderboard.setPosition((columns * 32) - 176, 32 * (rows + 0.5f));
+
+    hundreds.setTexture(icontextures["digits"]);
+    hundreds.setTextureRect(sf::IntRect(0, 0, 21, 32));
+    hundreds.setPosition(33, 32 * ((rows) + 0.5f) + 16);
+    tens.setTexture(icontextures["digits"]);
+    tens.setTextureRect(sf::IntRect(21 * 5, 0, 21, 32));
+    tens.setPosition(54, 32 * ((rows) + 0.5f) + 16);
+    ones.setTexture(icontextures["digits"]);
+    ones.setTextureRect(sf::IntRect(0, 0, 21, 32));
+    ones.setPosition(75, 32 * ((rows) + 0.5f) + 16);
+
+    timerMinutesTens.setTexture(icontextures["digits"]);
+    timerMinutesTens.setPosition((columns * 32) - 97, 32 * (rows + 0.5f) + 16);
+
+
+    timerMinutesOnes.setTexture(icontextures["digits"]);
+    timerMinutesOnes.setPosition((columns * 32) - 97 + 21, 32 * (rows + 0.5f) + 16);
+
+    timerSecondsTens.setTexture(icontextures["digits"]);
+    timerSecondsTens.setPosition((columns * 32) - 54, 32 * (rows + 0.5f) + 16);
+
+    timerSecondsOnes.setTexture(icontextures["digits"]);
+    timerSecondsOnes.setPosition((columns * 32) - 54 + 21, 32 * (rows + 0.5f) + 16);
+
+
+
+    MakeTiles();
+    RandomTileIds();
+    refreshCounter();
+
+}
+
+void TextureManager::load_board_config() {
+    std::ifstream config_file("board_config.cfg");
+    if (config_file.is_open()) {
+        config_file >> columns >> rows >> mines;
+
+        width = columns * 32;
+        height = (rows * 32) + 100;
+        tile_count = columns * rows;
+
+        config_file.close();
+    } else {
+        cout << "Unable to open board_config.cfg" << std::endl;
+    }
+}
+
+
+
+void TextureManager::RandomTileIds()
+{
+    tileids.clear();
+    vector<int> bits;
+    int randomnum;
+    bool getfree = false;
+    minesLeft = mines;
+
+    for (int c = 0; c < mines; c++)
+    {
+        while (!getfree)
+        {
+            randomnum = rand() % tile_count;
+            if (find(bits.begin(), bits.end(), randomnum) == bits.end())
+            {
+                bits.push_back(randomnum);
+                getfree = true;
+            }
+        }
+        getfree = false;
+    }
+
+    auto iter = bits.begin();
+    for (int d = 0; d < tile_count; d++)
+    {
+        iter = find(bits.begin(), bits.end(), d);
+        if (iter == bits.end())
+        {
+            tileids.push_back(false);
+        }
+        else
+        {
+            tileids.push_back(true);
+        }
+    }
+
+    setBombStates();
+    setIcons();
+    flagsavailable = mines; // initialize flagsavailable with the number of mines
+    refreshCounter();
+    tilesrevealed = 0;
+
+    game_lose = false;
+    game_over = false;
+}
+
+
+
+
+
+
+
+//instantiates the array of tiles, which will remain static over all games played during this instance
+void TextureManager::MakeTiles()
+{
+    float x = 0; float y = 0;
+    for (int c = 0; c < rows; c++)
+    {
+        for (int d = 0; d < columns; d++)
+        {
+            Tile t = Tile(icontextures, x, y);
+            tiles.push_back(t);
+            x += 32.f;
+        }
+        x = 0;
+        y += 32.f;
+    }
+}
+
+//calls setbombstate on each tile in tiles
+//for use ONLY when the game is reset, otherwise you'll just make a big mess
+void TextureManager::setBombStates()
+{
+    for (int c = 0; c < tile_count; c++)
+    {
+        tiles[c].setBombState(tileids[c]);
+    }
+}
+
+//assigns icons to each tile based on isbomb and nearby bombs
+void TextureManager::setIcons()
+{
+    for (int c = 0; c < tile_count; c++)
+    {
+        tiles[c].neighbors.clear();
+        for (int d = 0; d < 8; d++)
+        {
+            tiles[c].neighbors.push_back(nullptr);
+        }
+        if (c == 0) //tile is top left corner
+        {
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+            tiles[c].neighbors[7] = &(tiles[c + columns + 1]);
+        }
+        else if (c == columns - 1) // tile is top right corner
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[5] = &(tiles[c + columns - 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+        }
+        else if (c == tile_count - columns) // tile is bottom left corner
+        {
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[2] = &(tiles[c - columns + 1]);
+        }
+        else if (c == tile_count - 1) // tile is bottom right corner
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[0] = &(tiles[c - columns - 1]);
+        }
+        else if (c < columns) // tile is on the top
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[5] = &(tiles[c + columns - 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+            tiles[c].neighbors[7] = &(tiles[c + columns + 1]);
+        }
+        else if (c >= tile_count - columns) // tile is on the bottom
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[2] = &(tiles[c - columns + 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[0] = &(tiles[c - columns - 1]);
+        }
+        else if ((c % columns) == 0) // tile is on the left edge
+        {
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+            tiles[c].neighbors[7] = &(tiles[c + columns + 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[2] = &(tiles[c - columns + 1]);
+        }
+        else if (((c + 1) % columns) == 0) // tile is on the right edge
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[0] = &(tiles[c - columns - 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[5] = &(tiles[c + columns - 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+        }
+        else // tile is in the middle
+        {
+            tiles[c].neighbors[3] = &(tiles[c - 1]);
+            tiles[c].neighbors[4] = &(tiles[c + 1]);
+            tiles[c].neighbors[5] = &(tiles[c + columns - 1]);
+            tiles[c].neighbors[6] = &(tiles[c + columns]);
+            tiles[c].neighbors[7] = &(tiles[c + columns + 1]);
+            tiles[c].neighbors[2] = &(tiles[c - columns + 1]);
+            tiles[c].neighbors[1] = &(tiles[c - columns]);
+            tiles[c].neighbors[0] = &(tiles[c - columns - 1]);
+        }
+        tiles[c].updateIcon(icontextures);
+    }
+}
+
+
+
+void TextureManager::update(RenderWindow &window)
+{
+    Vector2i position = Mouse::getPosition(window);
+
+    int debugButtonX = (columns * 32) - 304;
+    int debugButtonY = 32 * (rows + 0.5f);
+    int happyFaceX = ((columns / 2.0) * 32) - 32;
+    int happyFaceY = 32 * (rows + 0.5f);
+    int pausePlayButtonX = (columns * 32) - 240;
+    int pausePlayButtonY = 32 * (rows + 0.5f);
+    int leaderboardButtonX = (columns * 32) - 176;
+    int leaderboardButtonY = 32 * (rows + 0.5f);
+
+    if (Mouse::isButtonPressed(Mouse::Left))
+    {
+        if (!leftButtonClicked)
+        {
+            leftButtonClicked = true;
+
+            if (position.y >= 0 && position.y < 512 && !game_over)
+            {
+                int x = (position.x / 32);
+                int y = (position.y / 32);
+
+                int pos = (y * columns) + x;
+                int count = 0;
+                if (tiles[pos].reveal(count))
+                {
+                    game_lose = true;
+                    game_over = true;
+                    int dummycount = 0;
+                    for (int c = 0; c < tile_count; c++)
+                    {
+                        if (tiles[c].isflagged && tiles[c].isbomb)
+                            tiles[c].toggleFlag();
+                        if (tiles[c].isbomb)
+                            tiles[c].reveal(dummycount);
+                    }
+                }
+                else tilesrevealed += count;
+            }
+            else if (position.y >= 512 && position.y < 576)
+            {
+                // Happy face button
+                if (position.x >= happyFaceX && position.x < happyFaceX + 64 &&
+                    position.y >= happyFaceY && position.y < happyFaceY + 64)
+                {
+                    RandomTileIds();
+                }
+                    // Debug button
+                else if (position.x >= debugButtonX && position.x < debugButtonX + 64 &&
+                         position.y >= debugButtonY && position.y < debugButtonY + 64 &&
+                         !game_over)
+                {
+                    debug();
+                }
+                    // Pause/Play button
+                else if (position.x >= pausePlayButtonX && position.x < pausePlayButtonX + 64 &&
+                         position.y >= pausePlayButtonY && position.y < pausePlayButtonY + 64)
+                {
+                    // Implement play/pause functionality
+                }
+                    // Leaderboard button
+                else if (position.x >= leaderboardButtonX && position.x < leaderboardButtonX + 64 &&
+                         position.y >= leaderboardButtonY && position.y < leaderboardButtonY + 64)
+                {
+                    // Implement leaderboard functionality
+                }
+            }
+        }
+    }
+    else
+    {
+        leftButtonClicked = false;
+    }
+
+
+    if (Mouse::isButtonPressed(Mouse::Right)) {
+        if (!rightMouseButtonPressed && !game_over) {
+            rightMouseButtonPressed = true;
+
+            Vector2i position = Mouse::getPosition(window);
+            if (position.x >= 0 && position.x <= 800) {
+                if (position.y >= 0 && position.y <= 512) {
+                    int x = (position.x / 32);
+                    int y = (position.y / 32);
+
+                    int pos = (y * columns) + x;
+                    refreshCounter();
+                    if (!tiles[pos].isflagged && !tiles[pos].isrevealed) {
+                        tiles[pos].toggleFlag();
+                        flagsavailable--;
+                        minesLeft--;
+                        cout << "minesLeft after placing flag: " << minesLeft << endl;
+
+                    } else if (!tiles[pos].isrevealed && tiles[pos].isflagged) { // Added the minesLeft != -1 check
+                        tiles[pos].toggleFlag();
+                        flagsavailable++;
+                        minesLeft++;
+                        cout << "minesLeft after removing flag: " << minesLeft << endl;
+                    }
+                }
+            }
+        }
+    } else {
+        rightMouseButtonPressed = false;
+    }
+
+    if (tilesrevealed == (tile_count - mines) && !game_lose) {
+        game_over = true;
+        for (int e = 0; e < tile_count; e++) {
+            if (tiles[e].isbomb && !tiles[e].isflagged) {
+                tiles[e].toggleFlag();
+            }
+        }
+    }
+
+    refreshCounter();
+    refreshSmiley();
+    draw(window);
+}
+
+//calls toggleDebug on each tile
+void TextureManager::debug()
+{
+    minesLeft = initialMinesLeft;
+    for (int c = 0; c < tile_count; c++)
+    {
+        tiles[c].toggleDebug();
+    }
+}
+
+//calls draw on each tile in tiles
+void TextureManager::draw(RenderWindow &window)
+{
+    for (int c = 0; c < tile_count; c++)
+        tiles[c].draw(window);
+
+    window.draw(smiley);
+    window.draw(debug_icon);
+    window.draw(pause);
+
+    window.draw(leaderboard);
+
+    window.draw(hundreds);
+    window.draw(tens);
+    window.draw(ones);
+    window.draw(timerMinutesTens);
+    window.draw(timerMinutesOnes);
+    window.draw(timerSecondsTens);
+    window.draw(timerSecondsOnes);
+
+}
+
+void TextureManager::refreshCounter() {
+    int displayMinesLeft = abs(minesLeft); // Use a temporary variable to store the absolute value of minesLeft
+
+    int hundredsDigit = displayMinesLeft / 100;
+    int tensDigit = (displayMinesLeft / 10) % 10;
+    int onesDigit = displayMinesLeft % 10;
+
+    if (minesLeft < 0) {
+        // Display the flag counter as negative
+        // Update the counter sprites with a minus sign
+        hundreds.setTextureRect(sf::IntRect(21 * 10, 0, 21, 32));
+    } else {
+        // Display the flag counter as positive
+        hundreds.setTextureRect(sf::IntRect(21 * hundredsDigit, 0, 21, 32));
+    }
+
+    // Update the tens and ones digits for both positive and negative numbers
+    tens.setTextureRect(sf::IntRect(21 * tensDigit, 0, 21, 32));
+    ones.setTextureRect(sf::IntRect(21 * onesDigit, 0, 21, 32));
+}
+
+
+
+
+
+
+void TextureManager::refreshSmiley()
+{
+    if (game_over)
+    {
+        if (game_lose)
+            smiley.setTexture(icontextures["lose"]);
+        else
+            smiley.setTexture(icontextures["win"]);
+    }
+    else smiley.setTexture(icontextures["happy"]);
+}
+
+void TextureManager::updateTimer() {
+    // Get elapsed time in seconds
+    currentTime = static_cast<int>(gameClock.getElapsedTime().asSeconds());
+
+    // Calculate the digits to display
+    int minutes = currentTime / 60;
+    int seconds = currentTime % 60;
+
+    // Calculate the digits for the timer
+    int minutesTensDigit = minutes / 10;
+    int minutesOnesDigit = minutes % 10;
+    int secondsTensDigit = seconds / 10;
+    int secondsOnesDigit = seconds % 10;
+
+    // Update the timer sprites with the appropriate digits
+    timerMinutesTens.setTextureRect(sf::IntRect(21 * minutesTensDigit, 0, 21, 32));
+    timerMinutesOnes.setTextureRect(sf::IntRect(21 * minutesOnesDigit, 0, 21, 32));
+    timerSecondsTens.setTextureRect(sf::IntRect(21 * secondsTensDigit, 0, 21, 32));
+    timerSecondsOnes.setTextureRect(sf::IntRect(21 * secondsOnesDigit, 0, 21, 32));
+}
--- main.cpp
+++ main.cpp
@@ -1,86 +1,141 @@
 #include <SFML/Graphics.hpp>
+
 #include <string>
-#include <vector>
+
 #include <fstream>
+
 #include <cctype>
-#include "Tile.h"
-#include "Board.h"
+
 #include "TextureManager.h"
-#include "WelcomeWindow.h"
-#include "LeaderboardWindow.h"
 
 using namespace sf;
 using namespace std;
+int currentState = 1;
 
-enum GameState {
-    WELCOME_SCREEN,
-    PLAYING,
-    LEADERBOARD,
-};
+void setText(sf::Text & text, float x, float y) {
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width / 2.0f,textRect.top + textRect.height / 2.0f);
+    text.setPosition(sf::Vector2f(x, y));
+}
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 576), "Minesweeper");
+void welcome_scene() {
+    sf::Context context;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Minesweeper", sf::Style::Close);
+    sf::Font font;
+    font.loadFromFile("font.ttf");
+    // Welcome Text
+    sf::Text welcomeText;
+    welcomeText.setFont(font);
+    welcomeText.setString("WELCOME TO MINESWEEPER!");
+    welcomeText.setCharacterSize(24);
+    welcomeText.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    welcomeText.setFillColor(sf::Color::White);
+    setText(welcomeText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 150);
 
-    GameState currentState = WELCOME_SCREEN;
+    // Input Prompt Text
+    sf::Text inputPromptText;
+    inputPromptText.setFont(font);
+    inputPromptText.setString("Enter your name:");
+    inputPromptText.setCharacterSize(20);
+    inputPromptText.setStyle(sf::Text::Bold);
+    inputPromptText.setFillColor(sf::Color::White);
+    setText(inputPromptText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 75);
 
-    // Initialize the TextureManager
-    TextureManager textureManager;
-    // Load textures using textureManager
+    // Input Text
+    sf::Text inputText;
+    inputText.setFont(font);
+    inputText.setString("");
+    inputText.setCharacterSize(18);
+    inputText.setStyle(sf::Text::Bold);
+    inputText.setFillColor(sf::Color::Yellow);
+    setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
 
-    // Display the Welcome window
-    WelcomeWindow welcomeWindow;
-
-    while (window.isOpen()) {
-        sf::Event event;
+    std::string playerName;
+    sf::Event event;
+    bool loop_exit = false;
+    while (!loop_exit) {
+        window.clear(sf::Color::Blue);
+        window.draw(welcomeText);
+        window.draw(inputPromptText);
+        window.draw(inputText);
+        window.display();
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-
-            // Handle user input for the current state
-            switch (currentState) {
-                case WELCOME_SCREEN:
-                    welcomeWindow.handleEvent(event);
-                    break;
-                case PLAYING:
-                    // Handle PLAYING state events here
-                    break;
-                case LEADERBOARD:
-                    // Handle LEADERBOARD state events here
-                    break;
+            if (event.type == sf::Event::TextEntered) {
+                if (std::isalpha(event.text.unicode) &&
+                    playerName.length() < 10) {
+                    char enteredChar = static_cast < char > (event.text.unicode);
+                    playerName += enteredChar;
+                    inputText.setString(playerName + "|");
+                    setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::BackSpace && !playerName.empty()) {
+                    playerName.pop_back();
+                    inputText.setString(playerName + "|");
+                    setText(inputText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 45);
+                }
+                else if (event.key.code == sf::Keyboard::Enter && !playerName.empty()) {
+                    playerName[0] = std::toupper(playerName[0]);
+                    for (size_t i = 1; i < playerName.length(); ++i) {
+                        playerName[i] = std::tolower(playerName[i]);
+                    }
+                    //currentState = 2;
+                    loop_exit = true;
+                    window.close();
+                }
             }
         }
+    }
 
-        window.clear();
 
-        switch (currentState) {
-            case WELCOME_SCREEN:
-                welcomeWindow.draw(window);
-                break;
-            case PLAYING:
-                // Create the Board using the TextureManager
-                Board board(textureManager);
+};
 
-                // Run the game loop
-                while (!game_over) {
-                    // Process user input
 
-                    // Update the board state
-
-                    // Draw the board
-                    board.draw(window);
-                }
-
-                currentState = LEADERBOARD;
-                break;
-            case LEADERBOARD:
-                // Display the Leaderboard window
-                LeaderboardWindow leaderboardWindow;
-                leaderboardWindow.draw(window);
-                break;
+void play_scene() {
+    TextureManager game_init;
+    game_init.load_board_config();
+    sf::RenderWindow window(sf::VideoMode(game_init.width, game_init.height), "Minesweeper", sf::Style::Close);
+    sf::Event event;
+    bool loop_exit = false;
+    sf::Clock clock;
+    while (!loop_exit) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+                loop_exit = true;
+            }
         }
-
+        // Calculate the elapsed time since the last loop iteration
+        sf::Time elapsed = clock.restart();
+        // Update the timer
+        game_init.updateTimer();
+        window.clear(sf::Color::White);
+        game_init.update(window);
         window.display();
+    }
+}
+
+
+
+
+
+
+
+int main() {
+    switch (currentState) {
+        case 1:
+            welcome_scene();
+            play_scene();
+            break;
+        case 2:
+            play_scene();
+            break;
+        default:
+            break;
     }
 
     return 0;
